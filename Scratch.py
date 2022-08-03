376f13dd7633d0577a99e8c454473c33d1c2d901
datastumbler <datastumbler@gmail.com>
2022-08-03 11:25:06 +0100

    Initial commit.
    Initial commit.

---
+++ Scratch.py
@@ -0,0 +1,6 @@
+In [178]: d[d['Text'].str.contains('AWAIT')]
+Out[178]:
+  Event                   Text
+0     A  something/AWAIT hello
+2     C          AWAITING SHIP
+3     D           yes NO AWAIT
